7952dbf52af0fcb94de9e511b2a8bedff47cae4e
cmjeff <jeff8wong@live.ca>
2023-01-27 01:56:36 -0800

    Initial Commit

---
+++ Bus Stop Finder/Bus_Stop_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bus_Stop_FinderApp.swift
+//  Bus Stop Finder
+//
+//  Created by jeff wong on 2023-01-27.
+//
+
+import SwiftUI
+
+@main
+struct Bus_Stop_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
